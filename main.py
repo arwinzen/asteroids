90aaa265a5bce903c356c6b9f761f4a435d99622
arwinzen <arwingoo90@gmail.com>
2025-06-09 19:28:48 +0800

    add player triangle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import *
 from constants import *
 
 def main():
@@ -6,16 +7,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
         # limit framerate to 60
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
